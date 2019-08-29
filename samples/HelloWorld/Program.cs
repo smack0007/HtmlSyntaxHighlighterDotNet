1f6b1681eab7c799bac75039cde00444a8a0c34b
smack0007 <zachary.snow@gmail.com>
2019-08-29 15:59:53 +0200

    Initial commit.

---
+++ samples/HelloWorld/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using RoslynHtmlSyntaxHighlighter;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var source =
+@"using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+  public static void Main()
+  {
+    foreach (var i in Fibonacci().Take(20))
+    {
+      Console.WriteLine(i);
+    }
+  }
+
+  private static IEnumerable<int> Fibonacci()
+  {
+    int current = 1, next = 1;
+
+    while (true) 
+    {
+      yield return current;
+      next = current + (current = next);
+    }
+  }
+}
+";
+
+            var syntaxHtml = HtmlSyntaxHighlighter.TransformCSharp(source);
+
+            var html =
+$@"<!DOCTYPE html>
+<html>
+    <head>
+        <link rel=""stylesheet"" type=""text/css"" href=""RoslynHtmlSyntaxHighlighter.css"">
+        <style>
+            code {{
+                display: block;
+                background-color: #1E1E1E;
+                color: white;
+            }}
+        </style>
+    </head>
+    <body>
+        <code>
+            <pre>{syntaxHtml}</pre>  
+        </code>
+    </body>
+</html>";
+
+            File.WriteAllText("index.html", html);
+        }
+    }
+}
---
+++ src/RoslynHtmlSyntaxHighlighter/HtmlSyntaxHightlighter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynHtmlSyntaxHighlighter
+{
+    public class HtmlSyntaxHighlighter
+    {
+        public static string TransformCSharp(string source)
+        {
+            var tree = CSharpSyntaxTree.ParseText(source);
+
+            var syntaxWalker = new CSharpSyntaxWalker();
+            syntaxWalker.Highlight((CompilationUnitSyntax)tree.GetRoot());
+
+            return syntaxWalker.ToString();
+        }
+
+        private class CSharpSyntaxWalker : Microsoft.CodeAnalysis.CSharp.CSharpSyntaxWalker
+        {
+            public StringBuilder _buffer;
+
+            public CSharpSyntaxWalker(int initialBufferCapacity = 1024)
+                : base(SyntaxWalkerDepth.Token)
+            {
+                _buffer = new StringBuilder(initialBufferCapacity);
+            }
+
+            public override string ToString() => _buffer.ToString();
+
+            public void Highlight(SyntaxNode node)
+            {
+                _buffer.Append("<span class=\"RoslynHtmlSyntaxHighlighter\">");
+                Visit(node);
+                _buffer.Append("</span>");
+            }
+
+            public override void VisitToken(SyntaxToken token)
+            {
+                if (token.HasLeadingTrivia)
+                {
+                    foreach (var trivia in token.LeadingTrivia)
+                        _buffer.Append(trivia);
+                }
+
+                _buffer.Append($"<span class=\"");
+
+                if (token.IsKeyword())
+                {
+                    _buffer.Append("keyword keyword-");
+                    _buffer.Append(token);
+                }
+                else if (token.IsKind(SyntaxKind.IdentifierToken))
+                {
+                    _buffer.Append("identifier");
+
+                    if (token.Parent is ClassDeclarationSyntax)
+                    {
+                        _buffer.Append(" identifier-class");
+                    }
+                    else if (token.Parent is MethodDeclarationSyntax)
+                    {
+                        _buffer.Append(" identifier-method");
+                    }
+                }
+                else
+                {
+                    _buffer.Append("token");
+                }
+
+                
+                _buffer.Append("\">");
+                _buffer.Append(token);
+                _buffer.Append("</span>");
+
+                if (token.HasTrailingTrivia)
+                {
+                    foreach (var trivia in token.TrailingTrivia)
+                        _buffer.Append(trivia);
+                }
+            }
+        }
+    }
+}
