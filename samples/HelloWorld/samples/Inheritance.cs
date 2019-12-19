6e89747ba7cc7d9e02839807a296379f35dbfe18
smack0007 <zachary.snow@gmail.com>
2019-12-19 17:03:15 +0100

    Added Inheritance example and changed Operators example.

---
+++ samples/HelloWorld/samples/Inheritance.cs
@@ -0,0 +1,12 @@
+﻿class Base<T>
+{
+    public void WriteClassName() => Console.WriteLine(typeof(T).Name);
+}
+
+class Foo : Base<Foo>
+{
+}
+
+class Bar
+{
+}
--- samples/HelloWorld/samples/Operators.cs
+++ samples/HelloWorld/samples/Operators.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿public const string MyVariable1 = nameof(MyVariable1);
 
-namespace HelloWorld.samples
+public static void Main()
 {
-    public static class Progam
-    {
-        public const string MyVariable1 = nameof(MyVariable1);
-
-        public static void Main()
-        {
-            var type1 = typeof(Program);
-            var default1 = default(int);
-        }
-    }
+    var type1 = typeof(Program);
+    var default1 = default(int);
 }
